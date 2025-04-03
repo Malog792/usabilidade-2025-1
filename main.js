fe9d30325bb9a46957b6ad4cb4a298200fe50e41
Maurício Lourenço de Aguiar - 82212145 <82212145@ulife.com.br>
2025-04-03 11:41:35 -0300

    export com funções e modularidade

--- main.js
+++ main.js
@@ -1,45 +1,11 @@
+import { decrypt, encrypt } from "./crypto.js";
+import { areaCirculo } from "./matematica.js";
 
-/*
-for(let i=0; i<10; i++){
-    console.log("oi povo")
-    console.log("oi turma")
-}
-    
-let idade = 20
+let segredo = encrypt("Alguma coisa")
+console.log("Segredo:", segredo);
 
-console.log("valor da idade "+idade);
-*/
+let original = decrypt(segredo)
+console.log("original:", original);
 
-/*   let idade = 20
-
-    console.log("idade ", idade);
-
-    function fazAlgo() {
-        console.log("Estou realizando a tarefa...")
-        console.log("Estou realizando a tarefa...")
-    }
-
-    console.log(fazAlgo())
-    fazAlgo()
-    */
-
-    let contador = 1
-
-    
-
-   
-    let id = setInterval(function() {
-        contador++
-        console.log(contador)
-    }, 1000)
-
-    //console.log("id",id)
-
-    
-    setTimeout(function () {
-        clearInterval(id)
-    },4000)
-
-    server.get("/", (req,res) =>{
-        
-    })
+let area = areaCirculo(10)
+console.log("area: ", area);
---
+++ matematica.js
@@ -0,0 +1,5 @@
+const PI = 3.1416
+
+export function areaCirculo(raio){
+    return PI*raio*raio
+}
